a2fa92af0982dcd7abee58578b6f2c996571cc0f
Michael <michael.jesus305@gmail.com>
2021-01-18 23:07:39 -0300

    :fire: remove PresentationTests

--- RickAndMorty/InfrastructureTests/Stubs/URLProtocolStub.swift
+++ RickAndMorty/InfrastructureTests/Stubs/URLProtocolStub.swift
@@ -9,6 +9,8 @@ import Foundation
 
 class URLProtocolStub: URLProtocol {
     static var emit: ((URLRequest) -> Void)?
+    static var response: HTTPURLResponse?
+    static var data: Data?
 
     static func observeRequest(completion: @escaping (URLRequest) -> Void) {
         URLProtocolStub.emit = completion
@@ -24,6 +26,15 @@ class URLProtocolStub: URLProtocol {
 
     override open func startLoading() {
         URLProtocolStub.emit?(request)
+
+        if let data = URLProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let response = URLProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+
+        client?.urlProtocolDidFinishLoading(self)
     }
 
     override open func stopLoading() {}
