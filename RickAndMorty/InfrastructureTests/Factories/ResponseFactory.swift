f656d2876c7c8a9606aa653029ae6fb8173eecb2
Michael <michael.jesus305@gmail.com>
2021-01-18 23:03:31 -0300

    create response factory

---
+++ RickAndMorty/InfrastructureTests/Factories/ResponseFactory.swift
@@ -0,0 +1,18 @@
+//
+//  ResponseFactory.swift
+//  InfrastructureTests
+//
+//  Created by Michael  on 18/01/21.
+//
+
+import Foundation
+
+final class ResponseFactory {
+    static func make(statusCode: Int = 200) -> HTTPURLResponse? {
+        guard let url = URLComponentsFactoryTests.makeUrlComponents().url else {
+            return nil
+        }
+
+        return HTTPURLResponse(url: url, statusCode: statusCode, httpVersion: nil, headerFields: nil)
+    }
+}
